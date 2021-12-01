bca61371cd9900851703c81d2da8f85c7fbcf896
IsaiGallegos <86752534+IsaiGallegos@users.noreply.github.com>
2021-11-30 19:15:51 -0600

    se subieron 4 archivos

---
+++ Camara.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    public GameObject personaje;
+
+    public int ejex = 0;
+
+    public int ejey = 0;
+
+    public int ejez = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Personaje.transform.position + new Vector3(ejex,ejey,ejez);
+    }
+}
---
+++ Control.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    public int Velocidad = 0;
+    public float giro = 0;
+    public float Horizontal = 0;
+    public float vertical = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Horizontal = Input.GetAxis(Horizontal);
+        vertical = Input.GetAxis("Vertical");
+        //ASIGNAR VELOCIDAD EN EJE Z
+        transform.Translate(Vector3.forward * Time.deltaTime * Velocidad * Vertical);
+        //ASIGNAR VELOCIDAD EN EJE X
+        transform.Translate(Vector3.right * Time.deltaTime * giro * Horizontal);
+    }
+}
